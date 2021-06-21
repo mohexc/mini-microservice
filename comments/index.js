cf99e6215a6981fc749eddd714b64381a3b0480c
nanut <galunyu@gmail.com>
2021-06-21 21:24:22 +0700

    added server-service comments, posts

---
+++ comments/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+const app = express()
+
+const commentsByPostId = {
+  postId: [
+    { commentId: "content", }
+  ],
+}
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('service comments running')
+})
+app.get('/comments', (req, res) => {
+  res.send(commentsByPostId)
+})
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || [])
+})
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex')
+  const { content } = req.body
+  const commentsList = commentsByPostId[req.params.id] || []
+  commentsList.push({ id: commentId, content })
+  commentsByPostId[req.params.id] = commentsList
+  res.status(201).json(commentsByPostId[req.params.id].find(comment => comment.id === commentId))
+})
+
+app.listen(4001, console.log('Server service posts on port 4001'))
---
+++ posts/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+const app = express()
+
+const posts = []
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('service posts running')
+})
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex')
+  const { title } = req.body
+  posts.push({ id, title })
+  res.status(201).json({ id, title })
+})
+
+app.listen(4000, console.log('Server service posts on port 4000'))
